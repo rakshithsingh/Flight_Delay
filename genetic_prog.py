c2e247dbb2536ecedef39a2443ae6d335de7616f
Rakshith Singh <rakshithsinghgm@gmail.com>
2017-01-28 01:33:43 +0530

    Add files via upload

--- genetic_prog.py
+++ genetic_prog.py
@@ -5,7 +5,9 @@ import pandas as pd
 import numpy as np
 
 # loading the data from a comma separated file (csv)
-data = pd.read_csv('train.csv')
+data = pd.read_csv('model2.csv',index_col=0)
+data.columns
+data.drop('UniqueCarrier',axis=1)
 
 #clean the data ( data preprocessing )
 data_shuffle = data.iloc[np.random.permutation(len(data))]
@@ -19,10 +21,10 @@ trainig_indices,validation_indices = training_indices, testing_indices = train_t
 
 # Genetic programming begins !!! 
 tpot = tp.TPOTClassifier(generations=5,verbosity=2) # optimization runs for 5 generations
-tpot.fit(tele.drop('Class',axis=1).loc[training_indices].values,tele.loc[training_indices,'Class'].values)
+tpot.fit(data.drop('Class',axis=1).loc[training_indices].values,data.loc[training_indices,'Class'].values)
 
 # Score accuracy
-tpot.score(tele.drop('Class',axis=1).loc[validation_indices].values,tele.loc[validation_indices,'Class'].values)
+tpot.score(data.drop('Class',axis=1).loc[validation_indices].values,data.loc[validation_indices,'Class'].values)
 
 # Export the pipeline (generations) as pipeline.py
 tpot.export('pipeline.py')
